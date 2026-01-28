becffbf708a22f9e0e121b6e2f3215d33160840b
aobetinho <aobetinhow@gmail.com>
2026-01-28 10:18:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
 )
 
 # ===========================================
